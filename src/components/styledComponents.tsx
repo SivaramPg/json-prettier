bd84293312264639e82568bcaf0ab26447577529
SivaramPg <sivarampg95@gmail.com>
2020-08-01 13:35:20 +0530

    styled components

---
+++ src/components/styledComponents.tsx
@@ -0,0 +1,16 @@
+import styled from 'styled-components';
+
+export const FlexContainer = styled.div`
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	position: relative;
+`;
+
+export const FlexRowContainer = styled(FlexContainer)`
+	flex-flow: row nowrap;
+`;
+
+export const FlexColumnContainer = styled(FlexContainer)`
+	flex-flow: column nowrap;
+`;
