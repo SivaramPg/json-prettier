24472c12b30142638079754a894bc69d6d2c07fc
SivaramPg <sivarampg95@gmail.com>
2020-08-01 14:50:03 +0530

    moved code around

--- src/components/PageTitle.tsx
+++ src/components/PageTitle.tsx
@@ -5,14 +5,14 @@ type PageTitleProps = {
 	title: string;
 };
 
-const H1 = styled.h1`
-	width: 100%;
-	text-align: center;
-	margin: 30px 0;
-`;
-
 const PageTitle: FunctionComponent<PageTitleProps> = ({ title }) => {
 	return <H1>{title}</H1>;
 };
 
 export default memo(PageTitle);
+
+const H1 = styled.h1`
+	width: 100%;
+	text-align: center;
+	margin: 30px 0;
+`;
