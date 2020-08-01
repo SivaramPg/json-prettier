04a210f2a2315c6b572e20c92ba1dd3c36d35a90
SivaramPg <sivarampg95@gmail.com>
2020-08-01 13:35:27 +0530

    page title component

---
+++ src/components/PageTitle.tsx
@@ -0,0 +1,18 @@
+import React, { memo, FunctionComponent } from 'react';
+import styled from 'styled-components';
+
+type PageTitleProps = {
+	title: string;
+};
+
+const H1 = styled.h1`
+	width: 100%;
+	text-align: center;
+	margin: 30px 0;
+`;
+
+const PageTitle: FunctionComponent<PageTitleProps> = ({ title }) => {
+	return <H1>{title}</H1>;
+};
+
+export default memo(PageTitle);
