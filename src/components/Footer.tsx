0ff77b40ba06faa314adf668b395245062beed01
SivaramPg <sivarampg95@gmail.com>
2020-08-01 13:35:37 +0530

    footer component

---
+++ src/components/Footer.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Footer = () => {
+	return (
+		<FooterContainer>
+			<p>Made by Sivaram Pandariganthan</p>
+		</FooterContainer>
+	);
+};
+
+export default Footer;
+
+const FooterContainer = styled.footer`
+	width: 100%;
+	height: 70px;
+
+	display: flex;
+	justify-content: center;
+	align-items: center;
+
+	flex-flow: row nowrap;
+	color: white;
+	background-color: black;
+`;
